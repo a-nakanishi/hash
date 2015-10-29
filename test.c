5f91d04351d4ae17a576905a1bd8a114b60193ef
a-nakanishi <a-nakanishi@osaka-u.ac.jp>
2015-10-29 15:48:12 +0900

    make-o

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <stdint.h>
 
 #include "BloomFilter.h"
 #include "HashTable.h"
